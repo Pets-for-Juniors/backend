9b2d5182c80287f8937f941f9fca0e7a60c9cdef
Sofi <nordpress-spb5@yandex.ru>
2024-09-19 17:00:12 +0300

    fix: #32 change value of maxAge

--- animals/constans.py
+++ animals/constans.py
@@ -3,5 +3,5 @@ import sys
 age_data = [
         {'title': "Молодые", 'minAge': 0, 'maxAge': 2},
         {'title': "В самом расцвете сил", 'minAge': 3, 'maxAge': 6},
-        {'title': "Пожилые", 'minAge': 7, 'maxAge': sys.maxsize}
+        {'title': "Пожилые", 'minAge': 7, 'maxAge': 1000}
     ]
